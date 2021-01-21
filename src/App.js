7dfb3f657121ab20388fa17d6cf9742fd1490758
HaxagonusD <jquezad1@binghamton.edu>
2021-01-20 19:12:09 -0500

    Added initial stocks

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import "./App.css";
 import { useEffect, useState } from "react";
 import Stock from "./components/Stock";
 import Search from "./components/Search";
+import getStock from "./services/getStock";
 /** @jsxImportSource @emotion/react */
 import tw from "twin.macro";
 
 function App() {
   const [stocks, setStocks] = useState([]);
   console.log(stocks);
+  useEffect(() => {
+    (async () => {
+      const fb = await getStock("fb");
+      const snap = await getStock("snap");
+      const shop = await getStock("shop");
+      const f = await getStock("f");
+      const aapl = await getStock("aapl");
+      setStocks([fb, f, snap, shop, aapl]);
+    })();
+  }, []);
   return (
     <div
       tw=" flex flex-col justify-center h-full bg-black text-white"
